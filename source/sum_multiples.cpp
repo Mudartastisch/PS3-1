0b4f46e902762de59042924cc8c1ac898d4a631a
Kai Lorenz <kai.lorenz@uni-weimar.de>
2020-05-07 12:18:11 +0200

    1.15 input sanitation

--- source/sum_multiples.cpp
+++ source/sum_multiples.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
+#include <cmath>
 
-//"char** is a pointer to a char*. Making it a pointer to a pointer to a char"
-int main(int argc, char** argv) {
+//"char** is a pointer to a char*. Making it a pointer to a pointer to a char" enabling me to use cli arguments
+int main(int argc, char **argv)
+{
     int sum = 0;
     unsigned int target;
-    if(argv[1]){
+    if (argv[1])
+    {                           //check for existance
         target = atoi(argv[1]); //http://www.cplusplus.com/reference/cstdlib/atoi/ String to int conversion
-    for (int i = 1; i <= target ; i++){
-        if (i % 3 == 0 || i % 5 == 0){
-            sum = sum + i;
-        }
-    }
+        if (target != 0)
+        { //check if its an int
+            for (int i = 1; i <= target; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
 
-    std::cout << sum << "\n";
+            std::cout << sum << "\n";
+        }
+        else
+        {
+            std::cout << "Usage: sum-m <int> (no 0 or non-int)" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        //http://www.cplusplus.com/reference/cstdlib/exit/
     }
-    else{ std::cout << "Usage: sum-m <int>" << std::endl; exit (EXIT_FAILURE);}
+    else
+    {
+        std::cout << "Usage: sum-m <int>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    //http://www.cplusplus.com/reference/cstdlib/exit/
 }
